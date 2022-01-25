14946b8efa3247b07ba62300929bb0ebfeef2918
scoolnik <n.martyushov.sas@gmail.com>
2022-01-25 17:14:34 +0500

    Дописал ModsLoader Добавил класс для простых тестов производительности

---
+++ Assets/Scripts/SimplePerformanceTestingTool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+class SimplePerformanceTestingTool : MonoBehaviour
+{
+	public List<double> results = new List<double>() { 0, 0 };
+	private Stopwatch sw = new Stopwatch();
+	private ModsLoader modsModel = new ModsLoader();
+
+	public void TestArray()
+	{
+		Action task = () => modsModel.TryLoadBundle(@"C:\Users\Пользователь\AppData\LocalLow\Zdorovtsov\SiegeUp!\mods\Simple decorations\windows");
+		AssetBundle.UnloadAllAssetBundles(true);
+
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+
+		sw.Restart();
+		for (int i=0; i< 100; i++)
+		{
+			sw.Start();
+			task.Invoke();
+			sw.Stop();
+			AssetBundle.UnloadAllAssetBundles(true);
+		}
+		sw.Stop();
+		results[0] = sw.Elapsed.TotalMilliseconds;
+	}
+}
+
+[CustomEditor(typeof(SimplePerformanceTestingTool))]
+class SimplePerformanceTestingToolGUI : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+		if (GUILayout.Button("Test array"))
+		{
+			(target as SimplePerformanceTestingTool).TestArray();
+		}
+	}
+}
---
+++ Assets/SiegeUp module/ModsLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+class ModsLoader
+{
+	private const string ModsFolderName = "mods";
+	private const string MetaFileName = "meta.json";
+
+	public List<SiegeUpModBase> GetInstalledMods()
+	{
+		List<SiegeUpModBase> mods = new List<SiegeUpModBase>();
+
+		string modsPath = Path.Combine(Application.persistentDataPath, ModsFolderName);
+		string platformName = GetCurrentPlatformName();
+		if (platformName == null)
+			return mods;
+		var modsFolders = Directory.GetDirectories(modsPath);
+		foreach (var folder in modsFolders)
+		{
+			var mod = TryLoadBundle(Path.Combine(folder, platformName));
+			if (mod == null)
+				continue;
+			mods.Add(mod);
+		}
+		return mods;
+	}
+
+	public SiegeUpModBase TryLoadBundle(string path)
+	{
+		var loadedAssetBundle = AssetBundle.LoadFromFile(path);
+		if (loadedAssetBundle == null)
+		{
+			Debug.Log("Failed to load AssetBundle from " + path);
+			return null;
+		}
+		return loadedAssetBundle.LoadAllAssets<SiegeUpModBase>()[0];
+	}
+
+	private string GetCurrentPlatformName()
+	{
+		return Application.platform switch
+		{
+			RuntimePlatform.WindowsPlayer | RuntimePlatform.WindowsEditor => PlatformShortName.Windows.ToString(),
+			RuntimePlatform.Android => PlatformShortName.Android.ToString(),
+			RuntimePlatform.LinuxPlayer | RuntimePlatform.LinuxEditor => PlatformShortName.Linux.ToString(),
+			RuntimePlatform.OSXPlayer | RuntimePlatform.OSXEditor => PlatformShortName.MacOS.ToString(),
+			RuntimePlatform.IPhonePlayer => PlatformShortName.IOS.ToString(),
+			_ => null
+		};
+	}
+}
